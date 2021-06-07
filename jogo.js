cb35dc85dd83c1f4290fddc48fed0fab9b630f97
cleuber-andrade <cleuber_binho@hotmail.com>
2021-06-07 17:33:12 -0300

    update 07/06/21
    17:33

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -18,7 +19,16 @@ function posicaoRandomica(){
     //remover o mosquito anterior(caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
-    } 
+
+
+        if (vidas > 3) {
+            window.location.href = 'fim_de_jogo.html'
+        } 
+        else{
+            document.getElementById('v' + vidas).src= "img/coracao_vazio.png"       
+            vidas++
+        }
+    }  
 
 
 
@@ -39,6 +49,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
     
